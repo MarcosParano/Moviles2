1508af921a2e5084cac18b7643d2f0ff99e4728e
MarcosParano <marcosparano@hotmail.com>
2026-08-26 19:31:17 -0300

    Segundo Commit

---
+++ Models/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moviles2.Models
+{
+    internal class UserProfile
+    {
+    }
+}
---
+++ ViewModels/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moviles2.ViewModels
+{
+    internal class ProfileViewModel
+    {
+    }
+}
